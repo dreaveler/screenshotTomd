2a568cfdfae9348f01f1c9007b5dac6c49a46a3b
dreaveler <2169448673@qq.com>
2025-07-01 21:07:03 +0800

    update

---
+++ api.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+ 
+client = OpenAI(
+    api_key="sk-A2iJAxUg01pb6wts29RvA6hcqo5EUcaq4h8w2mAZ7pKYkSLr",
+    base_url="https://api.moonshot.cn/v1",
+)
+def ask_moonshot(base64_image):
+    completion = client.chat.completions.create(
+        model = "moonshot-v1-8k",
+        messages = [
+            {"role": "system", "content": "你是一个精通markdown语言的专家，你擅长将图片中的数学公式转出为obsidian中的markdown格式，且你的输出是绝对正确的，且只会输出图片中的文字，对于图片中国的土星不做出任何响应，且不会增加、删改图片中的内容，且会自动进行换行。"},
+            {"role": "user", "content": [
+                {"type":"text" , "text":"你好，请将图片中的文字转成为markdow格式"},
+                {"type":"image_url","image_url":{"url": f"data:image/png;base64,{base64_image}"}}
+            ]}
+        ],
+        temperature = 0.3,
+    )
+    return completion.choices[0].message.content
--- getPhoto.py
+++ getPhoto.py
@@ -1,18 +1,30 @@
 from PIL import Image
 import win32clipboard
 import io
-def get_clipboard_image():
-    win32clipboard.OpenClipboard()
-    try:
-        if win32clipboard.IsClipboardFormatAvailable(win32clipboard.CF_DIB):
-            # 获取位图数据
-            data = win32clipboard.GetClipboardData(win32clipboard.CF_DIB)
-            # 转换为 PIL Image
-            image = Image.open(io.BytesIO(data))
-        else:
-            pass
-    finally:
-        win32clipboard.CloseClipboard()
-    return None
-def check_new_image():
-    win32clipboard.OpenClipboard()
+import time
+import base64
+class GetPhotoer:
+    def __init__(self):
+        self.data  = None
+        self.hash_num = None
+        self.image = None
+    def get_clipboard_image(self):
+        self.image = Image.open(io.BytesIO(self.data))
+    def get_clipboard_checksum(self):
+        try:
+            win32clipboard.OpenClipboard()
+            if win32clipboard.IsClipboardFormatAvailable(win32clipboard.CF_DIB):
+                self.data = win32clipboard.GetClipboardData(win32clipboard.CF_DIB)
+                self.hash_num = hash(self.data)
+        finally:
+            win32clipboard.CloseClipboard()
+    def wait_for_clipboard_image(self):
+        last_checksum = self.hash_num
+        current_checksum = None
+        while True:
+            self.get_clipboard_checksum()
+            current_checksum = self.hash_num
+            if last_checksum != current_checksum and current_checksum is not None:
+                self.get_clipboard_image()
+                last_checksum = current_checksum
+            time.sleep(0.5)
